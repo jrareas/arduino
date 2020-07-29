736c539f538036574376d7357012de92a80f3439
Jose Areas <jrareas@gmail.com>
2020-07-28 23:31:16 -0400

    First commit

---
+++ car/car.ino
@@ -0,0 +1,208 @@
+#include <RHReliableDatagram.h>
+#include <RH_NRF24.h>
+#include <SPI.h>
+
+#define CLIENT_ADDRESS 1
+#define SERVER_ADDRESS 2
+
+// Singleton instance of the radio driver
+RH_NRF24 driver;
+
+// Class to manage message delivery and receipt, using the driver declared above
+RHReliableDatagram manager(driver, SERVER_ADDRESS);
+
+int enA = 9;
+int en1 = 2;
+int en2 = 4;
+
+int enB = 5;
+int en3 = 7;
+int en4 = 6;
+
+int echoPin = 3;
+int trigPin = 11;
+
+bool debug = true;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(en1, OUTPUT);
+  pinMode(en2, OUTPUT);
+  pinMode(en3, OUTPUT);
+  pinMode(en4, OUTPUT);
+  Serial.begin(9600);
+  if (!manager.init()){
+    Serial.println("init failed");
+  } else {
+    Serial.println("init Success");
+  }
+}
+uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+
+void loop() {
+  //demoMode();
+  //return;
+  
+  if (manager.available())
+  {
+    // Wait for a message addressed to us from the client
+    uint8_t len = sizeof(buf);
+    uint8_t from;
+    if (manager.recvfromAck(buf, &len, &from))
+    {
+      Move(buf);
+    }
+  }
+}
+
+void Move(uint8_t buf[2]) {
+  if (debug) {
+      Serial.print("X: ");
+      Serial.println(buf[0]);
+
+      Serial.print("Y: ");
+      Serial.println(buf[1]);
+    
+  }
+  int wheelSpeed = 0;
+  if (buf[0] >= 132 || buf[0] <= 128) { // give a bit to make sure it considers the gap when it is not using
+    // just move forward or reverse
+    if ( buf[0] >= 132 ) {
+      wheelSpeed = map(buf[0], 132, 255, 0, 255);
+      MoveForward('Z', wheelSpeed);
+    } else {
+      wheelSpeed = map(buf[0], 132, 0, 0, 255);
+      Reverse('Z', wheelSpeed);
+    }
+  } else {
+    Stop('Z');
+  }
+
+  if (buf[1] >= 132 || buf[1] <= 128){
+    turn(buf[1]);
+  }
+}
+
+void turn(uint8_t buf) {
+    int wheelSpeed = 0;
+    
+    if ( buf >= 132 ) { // right
+      wheelSpeed = map(buf, 132, 255, 0, 255);
+      analogWrite(enA,wheelSpeed);
+      digitalWrite(en1, HIGH);
+      digitalWrite(en2, LOW);
+
+    } else { // left
+      wheelSpeed = map(buf, 132, 0, 0, 255);
+      digitalWrite(en3, HIGH);
+      digitalWrite(en4, LOW);
+      analogWrite(enB,wheelSpeed);
+    }
+}
+
+void MoveForward(char side, int wheelSpeed) {
+  switch(side) {
+    case 'A':
+      analogWrite(enA,wheelSpeed);
+      digitalWrite(en1, HIGH);
+      digitalWrite(en2, LOW);
+      break;
+    case 'B':
+      digitalWrite(en3, HIGH);
+      digitalWrite(en4, LOW);
+      analogWrite(enB,wheelSpeed);
+      break;
+    default:
+    analogWrite(enA,wheelSpeed);
+    analogWrite(enB,wheelSpeed);
+    digitalWrite(en1, HIGH);
+    digitalWrite(en2, LOW);
+    digitalWrite(en3, HIGH);
+    digitalWrite(en4, LOW);
+  }
+}
+
+void Wheel(bool reverse , char side, int wheelSpeed) {
+  Serial.println(wheelSpeed);
+  if (reverse){
+    Reverse(side, wheelSpeed);
+  } else {
+    MoveForward(side, wheelSpeed);
+  }
+}
+
+void Stop(char side) {
+  switch (side){
+    case 'A':
+      analogWrite(enA,0);
+      break;
+    case 'B':
+      analogWrite(enB,0);
+      break;
+    default:
+      analogWrite(enA,0);
+      analogWrite(enB,0);
+  }
+}
+
+void Reverse(char side, int wheelSpeed) {
+  if(side == 'A') {
+    analogWrite(enA,wheelSpeed);
+  } else {
+    analogWrite(enB,wheelSpeed);
+  }
+  switch(side) {
+    case 'A':
+      analogWrite(enA,wheelSpeed);
+      digitalWrite(en1, LOW);
+      digitalWrite(en2, HIGH);
+      break;
+    case 'B':
+      analogWrite(enB,wheelSpeed);
+      digitalWrite(en3, LOW);
+      digitalWrite(en4, HIGH);
+      break;
+    default:
+      analogWrite(enA,wheelSpeed);
+      analogWrite(enB,wheelSpeed);
+      digitalWrite(en1, LOW);
+      digitalWrite(en2, HIGH);
+      digitalWrite(en3, LOW);
+      digitalWrite(en4, HIGH);
+  }
+}
+
+void flip(int wheelSpeed) {
+  Wheel(true,'A', wheelSpeed);
+  Wheel(false,'B', wheelSpeed);
+}
+
+
+void demoMode() {
+  Wheel(false, 'Z', 255);
+  delay(1000);
+  Wheel(true, 'Z', 255);
+  delay(1000);
+  flip(255);
+  delay(1000);  
+}
+
+void manageAvoidance() {
+  long duration, distance;
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2)/29.1;
+  Serial.println(duration);
+  if(distance > 25) {
+    Wheel(true, 'Z', 255); // forward
+  } else {
+    flip(255);
+    delay(1000);
+    Wheel(true, 'Z', 255); // forward
+  }
+}
---
+++ oled_thermometer/oled_thermometer.ino
@@ -0,0 +1,290 @@
+//www.elegoo.com
+//2018.10.25
+
+#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <TimeLib.h>
+#include <DS3231.h>
+#include <dht_nonblocking.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Fonts/FreeSans9pt7b.h>
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+#define OLED_RESET 4
+
+#define NUMFLAKES     10
+
+#define LOGO_HEIGHT   4
+#define LOGO_WIDTH    4
+
+#define XPOS   0 // Indexes into the 'icons' array in function below
+#define YPOS   1
+#define DELTAY 2
+
+Adafruit_SSD1306 display(OLED_RESET);
+
+static const int DHT_SENSOR_PIN = 7;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+
+
+const unsigned char battery_full[] PROGMEM = {
+  B11111111,B11111110,
+  B11111111,B11111110,
+  B11111111,B11111111,
+  B11111111,B11111111,
+  B11111111,B11111111,
+  B11111111,B11111111,
+  B11111111,B11111110,
+  B11111111,B11111110
+};
+
+
+const unsigned char battery_empty[] PROGMEM = {
+  B11111111,B10000000,
+  B10000000,B11000000,
+  B10000000,B01000000,
+  B10000000,B11000000,
+  B11111111,B10000000
+};
+
+
+const unsigned char battery_33[] PROGMEM = {
+  B11111111,B11111110,
+  B11110000,B00000010,
+  B11110000,B00000011,
+  B11110000,B00000011,
+  B11110000,B00000011,
+  B11110000,B00000011,
+  B11110000,B00000010,
+  B11111111,B11111110
+};
+
+
+
+const unsigned char battery_half[] PROGMEM = {
+  B11111111,B11111110,
+  B11111110,B00000010,
+  B11111110,B00000011,
+  B11111110,B00000011,
+  B11111110,B00000011,
+  B11111110,B00000011,
+  B11111110,B00000010,
+  B11111111,B11111110
+};
+
+
+
+const unsigned char battery_75[] PROGMEM = {
+  B11111111,B11111110,
+  B11111111,B11100010,
+  B11111111,B11100011,
+  B11111111,B11100011,
+  B11111111,B11100011,
+  B11111111,B11100011,
+  B11111111,B11100010,
+  B11111111,B11111110
+};
+
+
+const char *monthName[12] = {
+  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+};
+
+DS3231  rtc(SDA, SCL);
+
+
+bool setuptime = false;
+
+void setTimeDevice() {
+  if (setuptime) {
+    setTime(__TIME__);
+    setDate(__DATE__);
+  }
+}
+
+bool setTime(const char *str)
+{
+  int Hour, Min, Sec;
+
+  if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) exit;
+
+  rtc.setTime(Hour, Min, Sec);
+
+  return true;
+}
+
+void setDate(const char *str)
+{
+  char Month[12];
+  int Day, Year;
+  uint8_t monthIndex;
+
+  if (sscanf(str, "%s %d %d", Month, &Day, &Year) != 3) exit;
+  
+  for (monthIndex = 0; monthIndex < 12; monthIndex++) {
+    if (strcmp(Month, monthName[monthIndex]) == 0) break;
+  }
+  if (monthIndex >= 12) exit;
+
+  rtc.setDate(Day, monthIndex + 1, Year);
+}
+
+/*
+ * Initialize the serial port.
+ */
+void setup( )
+{
+  Serial.begin(9600);
+  Wire.begin();
+  rtc.begin();
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  while (!Serial) ; // wait for Arduino Serial Monitor
+  
+  setTimeDevice();
+}
+
+/*
+ * Poll for a measurement, keeping the state machine alive.  Returns
+ * true if a measurement is available.
+ */
+static bool measure_environment( float *temperature, float *humidity )
+{
+  static unsigned long measurement_timestamp = millis( );
+
+  /* Measure once every four seconds. */
+  if( millis( ) - measurement_timestamp > 3000ul )
+  {
+    if( dht_sensor.measure( temperature, humidity ) == true )
+    {
+      measurement_timestamp = millis( );
+      return( true );
+    }
+  }
+
+  return( false );
+}
+
+void initDisplay() {
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  batterylevel();
+  writeDateToDisplay();
+  writeTimeToDisplay();
+}
+
+void writeTimeToDisplay() {
+  Time  t = rtc.getTime();
+  display.setCursor(0,0);
+  printDigits(t.hour);
+  display.print(":");
+  printDigits(t.min);
+  // take the seconds out until i figure out the delay issue
+  //display.print(":");
+  //printDigits(t.sec);
+}
+
+void writeDateToDisplay() {
+  display.setCursor(0,12);
+  display.print(rtc.getDOWStr());
+  display.print(", ");
+  display.print(rtc.getDateStr());
+}
+
+void printDigits(int digits) {
+ if (digits < 10) {
+  display.print('0'); 
+ }
+ 
+ display.print(digits);
+}
+
+/*
+ * Main program loop.
+ */
+void loop( )
+{
+  float temperature;
+  float humidity;
+  
+  /* Measure temperature and humidity.  If the functions returns
+     true, then a measurement is available. */
+  if( measure_environment( &temperature, &humidity ) == true )
+  {
+    initDisplay();
+    display.setCursor(0, 25);
+    display.print("T: ");
+    display.print(temperature);
+    display.print(" C");
+    display.setCursor(66,25);
+    display.print("H: "); 
+    display.print(humidity);
+    display.print(" %");
+    display.display();
+  }
+    
+  
+  delay(10);
+}
+
+byte batterylevel()
+{
+
+  int POS = 112;
+  int W = 16;
+  int H = 8;
+  //read the voltage and convert it to volt
+  double curvolt = double( readVcc() ) / 1000;
+  // check if voltge is bigger than 4.2 volt so this is a power source
+  if(curvolt > 4.6)
+  {
+    display.drawBitmap(POS,0,battery_full, W, H, WHITE);
+  }
+  if(curvolt <= 4.6 && curvolt > 4.2)
+  {
+    display.drawBitmap(POS,0,battery_75, W, H, WHITE);
+  }
+  if(curvolt <= 4.2 && curvolt > 3.8)
+  {
+    display.drawBitmap(POS,0,battery_half, W, H, WHITE);
+  }
+  if(curvolt <= 3.8 && curvolt > 3.0)
+  {
+    display.drawBitmap(POS,0,battery_33, W, H, WHITE);
+  }
+  if(curvolt <= 3.0)
+  {
+    display.drawBitmap(POS,0,battery_empty, W, H, WHITE);
+  }
+}
+//read internal voltage
+long readVcc() {
+  long result;
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA, ADSC));
+  result = ADCL;
+  result |= ADCH << 8;
+  result = 1126400L / result; // Back-calculate AVcc in mV
+  return result;
+}
+
+void drawBattery(const uint8_t *bitmap, uint8_t w, uint8_t h) {
+  int8_t f, icons[NUMFLAKES][3];
+
+  // Initialize 'snowflake' positions
+  for(f=0; f< NUMFLAKES; f++) {
+    icons[f][XPOS]   = random(1 - LOGO_WIDTH, display.width());
+    icons[f][YPOS]   = -LOGO_HEIGHT;
+    icons[f][DELTAY] = random(1, 6);
+    Serial.print(F("x: "));
+    Serial.print(icons[f][XPOS], DEC);
+    Serial.print(F(" y: "));
+    Serial.print(icons[f][YPOS], DEC);
+    Serial.print(F(" dy: "));
+    Serial.println(icons[f][DELTAY], DEC);
+  }
+}
